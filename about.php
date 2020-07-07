1b0b69496e81dfc306ca8beab23bbf96185c9c59
vibhabs <vibhabs3@gmail.com>
2020-07-07 19:08:20 +0530

    Easy Class Project

---
+++ about.php
@@ -0,0 +1,151 @@
+        <?php include('allhead.php'); ?>
+        <!-- Page Content -->
+        <div class="container" style="max-width: 1200px;">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h1 class="page-header">About
+        <small>Easy Class</small>
+        </h1>
+        <ol class="breadcrumb">
+        <li><a href=" index">Home</a>
+        </li>
+        <li class="active">About</li>
+     </ol>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+        <div class="col-md-6">
+        <img class="img-responsive" src="images/7fb1f193435815a86c8484f82b9589e1 - Copy.jpg" alt="">
+        </div>
+        <div class="col-md-6">
+        <h2>ABOUT EASY CLASS</h2>
+        <p>The <strong>Easy Class</strong> Website (web based application) is useful for the
+students, faculty, guest whomever likes to learn from web using E-Learn
+(Videos), as well Check result, schedules of assessment and all that task like
+event, news, students can find out list of fresh courses offered by them and
+admission procedure, discussion forum, fee structure etc. without going to
+institute. It provides the facility to the students or guest to have complete
+information about the institute.</p><br>
+<p>
+In this application, student can attend his\her missed classes from e-learn.</p>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h2 class="page-header">Our Faculties</h2>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/vij.png" alt="">
+        <div class="caption">
+        <h3>Dr.K Vijayakumar <br>
+            <small>HOD(MCA dept)</small>
+        </h3>
+        <p>MCA,Doctoral Research:Ph.D.(UoH)</p>
+        <ul class="list-inline">
+            <li><a href="www.facebook.com"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="www.linkedin.com"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="www.twitter.com"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/rmr.png" alt="">
+        <div class="caption">
+        <h3>Dr. CH Ram Mohan Reddy<br>
+            <small>Associate Professor</small>
+        </h3>
+        <p>M.Phil,Ph.D.</p>
+        <ul class="list-inline">
+            <li><a href="www.facebook.com"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="www.linkedin.com"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="www.twitter.com"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/srk.png" alt="">
+        <div class="caption">
+        <h3>M Sivaramakirshna <br>
+            <small>Associate Professor</small>
+        </h3>
+        <p>MCA</p>
+        <ul class="list-inline">
+            <li><a href="www.facebook.com"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="www.linkedin.com"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="www.twitter.com"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Our Student's</h2>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Akash.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Vibha.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/chai.jpg" alt="">
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- Footer -->
+        <footer>
+
+        <p class="footer text-center"> Easy Class 2019  <strong>MCA </strong> project Made by <strong>Vibha B.S and Sumedha Roy</strong></p>
+
+        </footer>
+
+        </div>
+        <!-- /.container -->
+        <style>
+
+        .footer{background: #000; padding: 10px 0px; color: #fff;position: fixed;left: 0; right: 0;bottom: -10px;}
+
+        </style>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Script to Activate the Carousel -->
+        <script>
+        $('.carousel').carousel({
+        interval: 5000 //changes the speed
+        })
+        </script>
+
+        </body>
+        </html>
---
+++ database.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$server = "localhost";
+$user = "root";
+$passdb = "";
+$db = "cc_db";
+$connect = mysqli_connect( $server, $user, $passdb, $db )or die( "Connection Error" );
+
+?>
